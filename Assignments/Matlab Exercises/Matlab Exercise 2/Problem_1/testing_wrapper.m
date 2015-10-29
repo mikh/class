fc0f7f08a3b4b8899a3beb588d1ef811c7298c47
Mikhail Andreev <mikha.andreev@gmail.com>
2015-10-29 14:14:34 -0400

    [matlab2-1b] rewrote testing wrapper to correctly get ccr and logloss

--- Assignments/Matlab Exercises/Matlab Exercise 2/Problem_1/testing_wrapper.m
+++ Assignments/Matlab Exercises/Matlab Exercise 2/Problem_1/testing_wrapper.m
@@ -1,9 +1,13 @@
-function [Y_pred, CCR,  TT, ETT ] = testing_wrapper( X, Y, N, W, t_max, TT, ETT )
+function [Y_pred, CCR, logloss,  TT, ETT ] = testing_wrapper( X, Y, N, W, t_max, TT, ETT )
     fprintf('Running testing...\t');
     t1 = clock;
-   
-    w_current = squeeze(W(t_max+1,:,:));
-    Y_pred = test(X, w_current, N);   
+    
+    
+    Y_pred = zeros(N, t_max);
+    for ii = 1:t_max
+        w_current = squeeze(W(ii+1,:,:));
+        Y_pred(:,ii) = test(X, w_current, N);
+    end
 
     t2 = clock;
     elapsed_time = etime(t2,t1);
@@ -14,19 +18,36 @@ function [Y_pred, CCR,  TT, ETT ] = testing_wrapper( X, Y, N, W, t_max, TT, ETT 
     fprintf('Getting CCR...\t');
     t1 = clock;
    
-    correct = 0;
-    for ii = 1:N
-        if Y_pred(ii) == Y(ii)
-            correct = correct + 1;
-        end
+    CCR = zeros(t_max, 1);
+    for ii = 1:t_max
+        CCR(ii) = sum(Y_pred==Y)/N*100;
     end
-    CCR = correct/N*100;
-   
+    
+    f2 = figure(2);
+    plot(1:t_max, CCR);
+    title('CCR over iterations');
+    xlabel('Iterations');
+    ylabel('CCR %%');
+    saveas(f2, 'CCR.png');
+    
+    logloss = zeros(t_max, 1);
+    for ii = 1:t_max
+        w_current = squeeze(W(ii+1,:,:));
+        logloss(ii) = calculate_logloss(X,Y,w_current,N);
+    end
+    
+    f3 = figure(3);
+    plot(1:t_max, logloss);
+    title('Logloss over iterations');
+    xlabel('Iterations');
+    ylabel('logloss');
+    saveas(f3, 'logloss.png');
+    
     t2 = clock;
     elapsed_time = etime(t2,t1);
     TT = TT + elapsed_time;
     ETT = ETT + elapsed_time;
     fprintf('Done. (%.2fs)\n', elapsed_time);
-    save('test_results', 'Y_pred', 'CCR');
+    save('test_results', 'Y_pred', 'CCR', 'logloss');
 end
 
