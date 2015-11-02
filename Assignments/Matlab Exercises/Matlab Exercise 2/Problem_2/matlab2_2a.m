e7737cf082369a17cb92391104e6830feca20b6f
Mikhail Andreev <mikha.andreev@gmail.com>
2015-11-02 17:51:30 -0500

    [matlab2-2f] Added code for 2f

--- Assignments/Matlab Exercises/Matlab Exercise 2/Problem_2/matlab2_2a.m
+++ Assignments/Matlab Exercises/Matlab Exercise 2/Problem_2/matlab2_2a.m
@@ -16,6 +16,7 @@ LOAD_DATA_FOR_PART_A = 1;
 RUN_PART_B = 0;
 RUN_PART_CD = 0;
 RUN_PART_E = 1;
+RUN_PART_F = 0;
 
 %% Load Data
 if LOAD_BASE_DATA == 1
@@ -156,6 +157,36 @@ if RUN_PART_E == 1
     fprintf('training time = %.2fs\n', training_time);
     fprintf('testing_time = %.2fs\n', testing_time);
     fid = fopen('part_e_confusion_matrix.txt', 'wt');
+    [r,c] = size(confusion_matrix);
+    for ii = 1:r
+        for jj = 1:c
+            fprintf(fid, '%d\t', confusion_matrix(ii,jj));
+        end
+        fprintf('\n');
+    end
+end
+
+%% part f
+if RUN_PART_F == 1
+    fprintf('Running part F\n');
+    if LOAD_DATA_FOR_PART_A == 1
+        fprintf('Loading base data...\t');
+        t1 = clock;
+        load 'word_freq_data.mat'
+        fprintf('Done. (%.2fs)\n', etime(clock, t1));
+    end
+
+    classes = cell(20,1);
+    class_sizes = zeros(20,1);
+    for ii = 1:20
+        [classes{ii}, class_sizes(ii), TT, ETT] = get_class_documents(X_wf_train, Y_train, N_train, ii, TT, ETT);
+    end
+    [SVM_classifiers, num_classifiers, training_time, TT, ETT] = train_ovo_svm(classes, class_sizes, 20, 'RBF', TT, ETT);
+    [CCR, confusion_matrix, testing_time, TT, ETT] = test_OVO_SVM(SVM_classifiers, num_classifiers, X_wf_test, Y_test, N_test, TT, ETT);
+    fprintf('CCR = %.2f\n', CCR);
+    fprintf('training time = %.2fs\n', training_time);
+    fprintf('testing_time = %.2fs\n', testing_time);
+    fid = fopen('part_f_confusion_matrix.txt', 'wt');
     [r,c] = size(confusion_matrix);
     for ii = 1:r
         for jj = 1:c
