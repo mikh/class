6a9faa954daa3cd394b475b15d467c96c78a907e
mikh <mikha.andreev@gmail.com>
2015-11-09 17:02:55 -0500

    [matlab3-2] Added code for part a, but it does not seem to be correct. There is currently an offset that cancels out all error which seems wrong. Needs additional info.

---
+++ Assignments/Matlab Exercises/Matlab Exercise 3/Problem_2/mikh_matlab3_2.m
@@ -0,0 +1,31 @@
+%% Init
+clear;
+clc;
+
+fprintf('Running matlab3 part 2...\n');
+t1 = clock;
+
+
+%% Load data
+fprintf('Loading linear_data.mat...\t');
+t2 = clock;
+load('linear_data.mat');
+fprintf('Done. (%.2fs)\n', etime(clock, t2));
+
+%% part a
+fprintf('Implementing OLS...\t');
+t2 = clock;
+W = (xData'*xData)^(-1) * xData' * yData;
+b = yData - xData * W;
+
+y_pred = (xData' .* W)' + b;
+MSE = 1/length(xData) * sum(abs(yData - y_pred).^2);
+MAD = 1/length(xData) * sum(abs(yData - y_pred));
+
+fprintf('MSE = %f\n', MSE);
+fprintf('MAD = %f\n', MAD);
+
+fprintf('Done. (%.2fs)\n', etime(clock, t2));
+
+%% Complete
+fprintf('matlab3-1 done. (%.2fs)\n', etime(clock,t1));
