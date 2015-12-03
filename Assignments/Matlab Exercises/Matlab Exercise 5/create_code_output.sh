1e5e7b0729b37af69f75a9c243d089143e7b4bb1
Mikhail Andreev <mikha.andreev@gmail.com>
2015-12-03 15:18:09 -0500

    [matlab5] Setup assignment files

---
+++ Assignments/Matlab Exercises/Matlab Exercise 5/create_code_output.sh
@@ -0,0 +1,14 @@
+#current_directory="/e/Digital_Library/data/Class/EC500/class/Assignments/Matlab Exercises/Matlab Exercise 4/"
+current_directory="/d/Digital_Library/data/Documents/Class/EC500_Learning_From_Data/Assignments/Matlab Exercises/Matlab Exercise 5/"
+output_files_txt="$current_directory""output.txt"
+output_directory="$current_directory""mikh_matlab5"
+
+rm -rf "$output_directory"
+mkdir "$output_directory"
+output_directory="$output_directory""/"
+
+while read p; do
+	echo $p
+	name=`basename $p`
+	cp "$current_directory""$p" "$output_directory""$name"
+done < "$output_files_txt"
